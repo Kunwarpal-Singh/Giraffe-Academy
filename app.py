716c65544612238dca30a293098350c4813a5960
Kunwarpal-Singh <kunwarpalsinghsethi13@gmail.com>
2023-08-09 21:12:16 -0400

    understanding logic and working of while loop

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ out_of_guesses = False
 while guess != secret_word and not(out_of_guesses):
     if guess_count < guess_limit:
         guess = input("Enter guess: ")
-        guess_count += 1
+        print(guess_count)
     else:
         out_of_guesses = True
 
