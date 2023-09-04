0f623d98ad065368f9b55885684b5301b579245b
Kunwarpal-Singh <kunwarpalsinghsethi13@gmail.com>
2023-09-04 18:55:09 -0400

    Built a basic translator

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-# 2D Lists
+# Translator
 
-number_grid = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-    [0]
-]
+# Giraffe Language
+# vowels -> g
+# ---------------------
+# dog -> dgg
+# cat -> cgt
 
-for row in number_grid:
-    for col in row:
-        print(col)
+def translate(phrase):
+    translation = " "
+    for letter in phrase:
+        if letter in "AEIOUaeiou":
+            translation = translation + "g"
+        else:
+            translation = translation + letter
+    return translation
+
+print(translate(input("Enter a phrase: ")))
