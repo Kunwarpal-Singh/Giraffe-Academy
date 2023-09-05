591b3a756ddee9642f61fe92b36fe71b589c6120
Kunwarpal-Singh <kunwarpalsinghsethi13@gmail.com>
2023-09-04 21:55:05 -0400

    Learnt to use try/except blocks

--- app.py
+++ app.py
@@ -1,21 +1,10 @@
-# Translator
+# Try/Except
 
-# Giraffe Language
-# vowels -> g
-# ---------------------
-# dog -> dgg
-# cat -> cgt
-
-def translate(phrase):
-    translation = " "
-    for letter in phrase:
-        if letter.lower() in "aeiou":
-            if letter.isupper():
-             translation = translation + "G"
-            else:
-             translation = translation + "g"
-        else:
-            translation = translation + letter
-    return translation
-
-print(translate(input("Enter a phrase: ")))
+try:
+    # value = 10/0
+    number = int(input("Enter a number: "))
+    print(number)
+except ZeroDivisionError as err:
+    print(err)
+except ValueError:
+    print("Invalid input")
